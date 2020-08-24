4ed840332f4799096c9511370a8cc5a25e06a074
Ivan Goncharov <Van4es7077@yandex.ru>
2020-08-24 22:05:03 +0300

    Add tasks Day1 solution

---
+++ src/Day1/Task5.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int age = 28; age < 100 ; age += 5) {
+            System.out.printf("Мой возраст %d\n", age);
+        }
+    }
+}
